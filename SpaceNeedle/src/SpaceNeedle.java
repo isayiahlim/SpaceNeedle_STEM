e1765ab31bbc778a7a309c20f8e4ec3f48e31c4e
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-09-30 08:50:55 -0700

    First Commit

--- SpaceNeedle/src/SpaceNeedle.java
+++ SpaceNeedle/src/SpaceNeedle.java
@@ -1,13 +1,29 @@
-
 /*
- * Name
- * Date
- * APCS Period
+ * Isayiah Lim
+ * 9/30/2021
+ * APCS Period 2
  * Space Needle Project
  * 
- * Program Description
+ * This program prints out an ASCII Space Needle, which can be scaled by changing the SCALE Integer
  */
 public class SpaceNeedle
 {
-    
+    //scale number
+	public static final Integer SCALE = 4;
+	
+	//main method, prints out the whole needle
+	public static void main(String[] args)
+    {
+    	needle();
+    }
+	
+	//prints out the spire and the thin needle in the middle
+    public static void needle()
+    {
+    	//makes the correct number of rows
+    	for(int i = 0; i < SCALE; i++)
+    	{
+    		
+    	}
+    }
 }
